1640ec2764a792c32d060bd1434a87fdd6e1b916
carl <44021312+achristmascarl@users.noreply.github.com>
2025-03-19 15:06:43 -0400

    fix install script compat with older jq versions (#156)

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ main() {
   echo "temp dir: $temp"
 
   echo "installing 🐸 rainfrog..."
-  release_json=$(curl -s https://api.github.com/repos/achristmascarl/rainfrog/releases/latest | jq)
-  binary=$(jq <<<$release_json | jq -r '.["assets"] | .[] | .name' | sed -u "s/^.*[.sha256]$//" | sed -u "s/[.]tar[.]gz//" | awk 'NF' | fzf --header "choose a binary from the latest rainfrog release:" --reverse)
+  release_json=$(curl -s https://api.github.com/repos/achristmascarl/rainfrog/releases/latest | jq .)
+  binary=$(jq . <<<$release_json | jq -r '.["assets"] | .[] | .name' | sed -u "s/^.*[.sha256]$//" | sed -u "s/[.]tar[.]gz//" | awk 'NF' | fzf --header "choose a binary from the latest rainfrog release:" --reverse)
   if [ -z "$binary" ]; then
     echo "no binary selected"
     exit 1
@@ -36,8 +36,8 @@ main() {
 
   # download binary and hash
   echo "downloading binary and hash..."
-  curl -fL $(jq <<<$release_json | jq -r ".assets[] | select(.name | contains(\"$binary.tar.gz\")) | .browser_download_url") >"$temp/$binary.tar.gz"
-  curl -fL $(jq <<<$release_json | jq -r ".assets[] | select(.name | contains(\"$binary.sha256\")) | .browser_download_url") >"$temp/$binary.sha256"
+  curl -fL $(jq . <<<$release_json | jq -r ".assets[] | select(.name | contains(\"$binary.tar.gz\")) | .browser_download_url") >"$temp/$binary.tar.gz"
+  curl -fL $(jq . <<<$release_json | jq -r ".assets[] | select(.name | contains(\"$binary.sha256\")) | .browser_download_url") >"$temp/$binary.sha256"
   current=$(pwd)
   cd $temp
   shasum -c "$temp/$binary.sha256"
