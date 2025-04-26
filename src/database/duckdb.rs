478583122d1ae52e9cfeae990936ca2e0e5f6699
achristmascarl <carl.z.liu@gmail.com>
2025-04-25 23:57:48 -0400

    query method, placehodlers

--- src/database/duckdb.rs
+++ src/database/duckdb.rs
@@ -10,7 +10,7 @@ use duckdb::{params, Config, Connection, Transaction};
 
 use crate::cli::{Cli, Driver};
 
-use super::{Database, Header, Headers, QueryResultsWithMetadata, QueryTask};
+use super::{Database, DbTaskResult, Header, Headers, QueryResultsWithMetadata, QueryTask, Rows};
 
 type TransactionTask<'a> = tokio::task::JoinHandle<(QueryResultsWithMetadata, Transaction<'a>)>;
 enum DuckDbTask<'a> {
@@ -19,6 +19,7 @@ enum DuckDbTask<'a> {
   TxPending((Transaction<'a>, QueryResultsWithMetadata)),
 }
 
+#[derive(Default)]
 pub struct DuckDbDriver<'a> {
   connection: Option<Arc<Connection>>,
   task: Option<DuckDbTask<'a>>,
@@ -37,9 +38,9 @@ impl Database for DuckDbDriver<'_> {
   // we only execute the first one and then drop the rest.
   fn start_query(&mut self, query: String) -> Result<()> {
     let (first_query, statement_type) = super::get_first_query(query, Driver::DuckDb)?;
-    let connection = self.connection.clone().unwrap();
-    let statement = connection.prepare(&first_query)?;
+    let connection = self.connection.clone().unwrap().try_clone()?;
     self.task = Some(DuckDbTask::Query(tokio::spawn(async move {
+      let mut statement = connection.prepare(&first_query).unwrap();
       let headers: Headers = statement
         .column_names()
         .iter()
@@ -50,42 +51,112 @@ impl Database for DuckDbDriver<'_> {
         })
         .collect();
 
-      let mut rows = statement.query([])?;
-      // let results = query_with_pool(pool, first_query.clone()).await;
-      // match results {
-      //   Ok(ref rows) => {
-      //     log::info!("{:?} rows, {:?} affected", rows.rows.len(), rows.rows_affected);
-      //   },
-      //   Err(ref e) => {
-      //     log::error!("{e:?}");
-      //   },
-      // };
-      // QueryResultsWithMetadata { results, statement_type: statement_type.clone() }
+      let mut results: Vec<Vec<String>> = Vec::new();
+      let rows = statement.query([]);
+      match rows {
+        Ok(mut rows) => {
+          while let Ok(Some(row)) = rows.next() {
+            let mut r: Vec<String> = Vec::new();
+            for i in 0..headers.len() {
+              let value = row.get::<_, Option<String>>(i);
+              if let Ok(Some(value)) = value {
+                r.push(value);
+              } else {
+                r.push(String::new());
+              }
+            }
+            results.push(r);
+          }
+          QueryResultsWithMetadata { results: Ok(Rows { headers, rows: results, rows_affected: None }), statement_type }
+        },
+        Err(e) => {
+          return QueryResultsWithMetadata { results: Err(eyre::Report::new(e)), statement_type };
+        },
+      }
     })));
     Ok(())
   }
 
-  fn abort_query(&mut self) -> Result<bool>;
+  fn abort_query(&mut self) -> Result<bool> {
+    if let Some(task) = self.task.take() {
+      match task {
+        DuckDbTask::Query(handle) => handle.abort(),
+        DuckDbTask::TxStart(handle) => handle.abort(),
+        _ => {},
+      };
+      Ok(true)
+    } else {
+      Ok(false)
+    }
+  }
 
-  async fn get_query_results(&mut self) -> Result<DbTaskResult>;
+  async fn get_query_results(&mut self) -> Result<DbTaskResult> {
+    let (task_result, next_task) = match self.task.take() {
+      None => (DbTaskResult::NoTask, None),
+      Some(DuckDbTask::Query(handle)) => {
+        if !handle.is_finished() {
+          (DbTaskResult::Pending, Some(DuckDbTask::Query(handle)))
+        } else {
+          let result = handle.await?;
+          (DbTaskResult::Finished(result), None)
+        }
+      },
+      Some(DuckDbTask::TxStart(handle)) => {
+        if !handle.is_finished() {
+          (DbTaskResult::Pending, Some(DuckDbTask::TxStart(handle)))
+        } else {
+          let (result, tx) = handle.await?;
+          let rows_affected = match &result.results {
+            Ok(rows) => rows.rows_affected,
+            _ => None,
+          };
+          (
+            DbTaskResult::ConfirmTx(rows_affected, result.statement_type.clone()),
+            Some(DuckDbTask::TxPending((tx, result))),
+          )
+        }
+      },
+      Some(DuckDbTask::TxPending((tx, results))) => (DbTaskResult::Pending, Some(DuckDbTask::TxPending((tx, results)))),
+    };
+    self.task = next_task;
+    Ok(task_result)
+  }
 
-  async fn start_tx(&mut self, query: String) -> Result<()>;
+  async fn start_tx(&mut self, query: String) -> Result<()> {
+    todo!()
+  }
 
-  async fn commit_tx(&mut self) -> Result<Option<QueryResultsWithMetadata>>;
+  async fn commit_tx(&mut self) -> Result<Option<QueryResultsWithMetadata>> {
+    todo!()
+  }
 
-  async fn rollback_tx(&mut self) -> Result<()>;
+  async fn rollback_tx(&mut self) -> Result<()> {
+    todo!()
+  }
 
-  async fn load_menu(&self) -> Result<Rows>;
+  async fn load_menu(&self) -> Result<Rows> {
+    todo!()
+  }
 
-  fn preview_rows_query(&self, schema: &str, table: &str) -> String;
+  fn preview_rows_query(&self, schema: &str, table: &str) -> String {
+    todo!()
+  }
 
-  fn preview_columns_query(&self, schema: &str, table: &str) -> String;
+  fn preview_columns_query(&self, schema: &str, table: &str) -> String {
+    todo!()
+  }
 
-  fn preview_constraints_query(&self, schema: &str, table: &str) -> String;
+  fn preview_constraints_query(&self, schema: &str, table: &str) -> String {
+    todo!()
+  }
 
-  fn preview_indexes_query(&self, schema: &str, table: &str) -> String;
+  fn preview_indexes_query(&self, schema: &str, table: &str) -> String {
+    todo!()
+  }
 
-  fn preview_policies_query(&self, schema: &str, table: &str) -> String;
+  fn preview_policies_query(&self, schema: &str, table: &str) -> String {
+    todo!()
+  }
 }
 
 impl DuckDbDriver<'_> {
